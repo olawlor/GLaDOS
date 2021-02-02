d3ccf1b276b92a72ffeffabfcb6eff7f6478b233
Orion Lawlor <lawlor@alaska.edu>
2021-02-01 23:06:47 -0900

    Fix: use relative links to x86_64 architecture headers, to allow efi headers without -Iinclude/efi

--- include/efi/efisetjmp.h
+++ include/efi/efisetjmp.h
@@ -2,7 +2,7 @@
 #define GNU_EFI_SETJMP_H
 
 #include "eficompiler.h"
-#include "efisetjmp_arch.h"
+#include "x86_64/efisetjmp_arch.h"
 
 extern UINTN setjmp(jmp_buf *env) __attribute__((returns_twice));
 extern VOID longjmp(jmp_buf *env, UINTN value) __attribute__((noreturn));
